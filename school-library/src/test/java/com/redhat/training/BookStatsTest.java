75f3cf3189ab8ecc9f289c34c025f37c2168b15e
nosnow111 <nosnow111@yahoo.com>
2021-03-13 12:41:40 -0500

    finish exercise

--- school-library/src/test/java/com/redhat/training/BookStatsTest.java
+++ school-library/src/test/java/com/redhat/training/BookStatsTest.java
@@ -19,9 +19,15 @@ public class BookStatsTest {
         // Then
         assertEquals(0, wordCount);
     }
-
     @Test
     public void countingWordsReturnsNumberOfWordsInBook() {
-        assertEquals(0, 1); // Replace this line with the actual test code...
+        // Given
+        Book book = new Book("someISBN", "this is the content");
+    
+        // When
+        double wordCount = BookStats.countWords(book);
+   
+        // Then
+        assertEquals(4, wordCount);
     }
 }
